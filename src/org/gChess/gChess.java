a752174c44f5424b41d48e86c230bbe152be6923
John Gunderman <johngunderman@gmail.com>
2009-04-13 12:54:20 -0700

    Created new Android Project for Eclipse

---
+++ src/org/gChess/gChess.java
@@ -0,0 +1,13 @@
+package org.gChess;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class gChess extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
