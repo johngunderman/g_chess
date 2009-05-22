e73652239bc1180207a18aa284510c871e891824
John Gunderman <johngunderman@gmail.com>
2009-05-21 23:25:21 -0700

    more getValidMoveLocations

--- src/org/gChess/Queen.java
+++ src/org/gChess/Queen.java
@@ -35,5 +35,54 @@ public class Queen extends ChessPiece {
 		}
 		return null;
 	}
+	
+	@Override
+	public ArrayList<Location> getValidMoveLocations() {
+		ArrayList<Location> locs = new ArrayList<Location>();
+		
+		ArrayList<Location> temp;
+		
+		temp = getBoard().locsInDirUntilBlocked(getLoc(), 
+				Location.BACK_LEFT, 
+				getColor());
+		locs.addAll(temp);
+		
+		temp = getBoard().locsInDirUntilBlocked(getLoc(), 
+				Location.BACK_RIGHT, 
+				getColor());
+		locs.addAll(temp);
+		
+		temp = getBoard().locsInDirUntilBlocked(getLoc(), 
+				Location.FORWARD_LEFT, 
+				getColor());
+		locs.addAll(temp);
+		
+		temp = getBoard().locsInDirUntilBlocked(getLoc(), 
+				Location.FORWARD_RIGHT, 
+				getColor());
+		locs.addAll(temp);
+		
+		temp = getBoard().locsInDirUntilBlocked(getLoc(), 
+				Location.FORWARD, 
+				getColor());
+		locs.addAll(temp);
+		
+		temp = getBoard().locsInDirUntilBlocked(getLoc(), 
+				Location.BACK, 
+				getColor());
+		locs.addAll(temp);
+		
+		temp = getBoard().locsInDirUntilBlocked(getLoc(), 
+				Location.RIGHT, 
+				getColor());
+		locs.addAll(temp);
+		
+		temp = getBoard().locsInDirUntilBlocked(getLoc(), 
+				Location.LEFT, 
+				getColor());
+		locs.addAll(temp);
+		
+		return locs;
+	}
 
 }
--- src/org/gChess/Rook.java
+++ src/org/gChess/Rook.java
@@ -55,5 +55,34 @@ public class Rook extends ChessPiece {
 		}
 		return moves;
 	}
+	
+	@Override
+	public ArrayList<Location> getValidMoveLocations() {
+		ArrayList<Location> locs = new ArrayList<Location>();
+		
+		ArrayList<Location> temp;
+		
+		temp = getBoard().locsInDirUntilBlocked(getLoc(), 
+				Location.FORWARD, 
+				getColor());
+		locs.addAll(temp);
+		
+		temp = getBoard().locsInDirUntilBlocked(getLoc(), 
+				Location.BACK, 
+				getColor());
+		locs.addAll(temp);
+		
+		temp = getBoard().locsInDirUntilBlocked(getLoc(), 
+				Location.RIGHT, 
+				getColor());
+		locs.addAll(temp);
+		
+		temp = getBoard().locsInDirUntilBlocked(getLoc(), 
+				Location.LEFT, 
+				getColor());
+		locs.addAll(temp);
+		
+		return locs;
+	}
 
 }
